296551b4c4a46798756db2a37475841a324fc19b
Stuart Langridge <sil@kryogenix.org>
2021-09-23 09:40:26 +0100

    initial import

---
+++ utm_no/__main__.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python3
+import sys
+import os
+import gi
+
+from . import url_handler
+
+gi.require_version('Gtk', '3.0')
+from gi.repository import GObject, Gtk, GLib, GdkPixbuf, Gdk
+
+gi.require_version('AppIndicator3', '0.1')
+from gi.repository import AppIndicator3 as AppIndicator
+
+APP_ID = 'utm_no'
+APP_NAME = 'utm_no'
+APP_LICENCE = """
+MIT License
+
+Copyright (c) 2021 Stuart Langridge
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+"""
+APP_VERSION = os.environ.get("SNAP_VERSION", "latest")
+
+
+class UTMNOIndicator(GObject.GObject):
+    def __init__(self):
+        GObject.GObject.__init__(self)
+
+        icon_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "icons"))
+        self.panel_eyes_closed_icon = os.path.abspath(os.path.join(icon_path, "panel-eyes-closed.svg"))
+        self.panel_eyes_left_icon = os.path.abspath(os.path.join(icon_path, "panel-eyes-left.svg"))
+        self.panel_eyes_right_icon = os.path.abspath(os.path.join(icon_path, "panel-eyes-right.svg"))
+        self.panel_eyes_half_right_icon = os.path.abspath(os.path.join(icon_path, "panel-eyes-half-right.svg"))
+        self.panel_disabled_icon = os.path.abspath(os.path.join(icon_path, "panel-eyes-disabled.svg"))
+        self.app_icon = os.path.abspath(os.path.join(icon_path, "utm_no.svg"))
+
+        self.ind = AppIndicator.Indicator.new(
+            APP_ID, self.panel_eyes_closed_icon,
+            AppIndicator.IndicatorCategory.HARDWARE)
+        self.ind.set_status(AppIndicator.IndicatorStatus.ACTIVE)
+
+        self.menu = Gtk.Menu()
+        self.ind.set_menu(self.menu)
+
+        self.mpaused = Gtk.CheckMenuItem.new_with_mnemonic("_Enabled")
+        self.mpaused.connect("toggled", self.toggle_enabled, None)
+        self.mpaused.set_active(True)
+        self.mpaused.show()
+        self.menu.append(self.mpaused)
+
+        mabout = Gtk.MenuItem.new_with_mnemonic("_About")
+        mabout.connect("activate", self.show_about, None)
+        mabout.show()
+        self.menu.append(mabout)
+
+        mquit = Gtk.MenuItem.new_with_mnemonic("_Quit")
+        mquit.connect("activate", self.quit, None)
+        mquit.show()
+        self.menu.append(mquit)
+
+        clipboard = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
+        clipboard.connect('owner-change', self.clipboardChanged)
+        # don't monitor PRIMARY selection, the middle-click clipboard
+        # primary = Gtk.Clipboard.get(Gdk.SELECTION_PRIMARY)
+        # primary.connect('owner-change', clipboardChanged)
+
+    def handleText(self, clipboard, text):
+        if url_handler.is_url(text.strip()):
+            # if the text is nothing but a URL, handle it always
+            new_text = url_handler.fix_text(text)
+        elif False:
+            # if the setting is on, process the whole text and handle all URLs within it
+            new_text = url_handler.fix_text(text)
+        else:
+            return text
+        if new_text == text:
+            return text
+
+        # The text has been changed, set it on the clipboard and flash the icon
+        clipboard.set_text(new_text, -1)
+        print("Overridden clipboard contents to", repr(new_text))
+        self.animate_icon()
+
+    def animate_icon(self, step=0):
+        STEPS = [
+            self.panel_eyes_right_icon,
+            self.panel_eyes_half_right_icon,
+            self.panel_eyes_closed_icon,
+            self.panel_eyes_left_icon
+        ]
+        # if we're done, bail without recursively going on to the next step
+        if step >= len(STEPS):
+            self.ind.set_status(AppIndicator.IndicatorStatus.ACTIVE)
+            return
+        self.ind.set_attention_icon_full(STEPS[step], f"{APP_NAME} altering clipboard")
+        if step == 0:
+            self.ind.set_status(AppIndicator.IndicatorStatus.ATTENTION)
+        GLib.timeout_add(150, self.animate_icon, step + 1)
+
+    def clipboardChanged(self, clipboard, owner_change):
+        if not self.mpaused.get_active(): return
+        # This should not infinitely loop, because we don't set the text
+        # unless we changed it, and if we changed it, it won't need
+        # changing again, so we won't set it a second time.
+        if clipboard.wait_is_text_available():
+            data = clipboard.wait_for_text()
+            if data:
+                ndata = data + ""  # cast to string
+                self.handleText(clipboard, ndata)
+
+    def toggle_enabled(self, widget, *args):
+        if widget.get_active():
+            self.ind.set_icon_full(self.panel_eyes_closed_icon, f"{APP_NAME} running")
+        else:
+            self.ind.set_icon_full(self.panel_disabled_icon, f"{APP_NAME} disabled")
+
+    def quit(self, *args):
+        GLib.timeout_add(100, lambda *args: Gtk.main_quit())
+
+    def show_about(self, *args):
+        dialog = Gtk.AboutDialog()
+        dialog.set_program_name(APP_NAME)
+        dialog.set_copyright('Stuart Langridge')
+        dialog.set_license(APP_LICENCE)
+        dialog.set_version(APP_VERSION)
+        dialog.set_website('https://kryogenix.org')
+        dialog.set_website_label('kryogenix.org')
+        dialog.set_comments("Remove tracking parameters from copied links")
+        dialog.set_logo(GdkPixbuf.Pixbuf.new_from_file(self.app_icon))
+        dialog.connect('response', lambda *largs: dialog.destroy())
+        dialog.run()
+
+    @staticmethod
+    def run():
+        Gtk.main()
+
+
+if __name__ == "__main__":
+    UTMNOIndicator().run()
---
+++ utm_no/url_handler.py
@@ -0,0 +1,229 @@
+"Handle all the URL stripping"
+
+import re
+import urllib.parse
+import unittest
+
+# from https://github.com/rknightuk/TrackerZapper/blob/main/TrackerZapper/AppDelegate.swift#L160
+STRIP_URL_QUERY_ELEMENTS_STARTS = [
+    "_bta_c", "_bta_tid", "_ga", "_hsenc", "_hsmi", "_ke", "_openstat",
+    "dm_i", "ef_id", "epik", "fbclid", "gclid", "gclsrc", "gdffi", "gdfms",
+    "gdftrk", "hsa_", "igshid", "matomo_", "mc_", "mkwid", "msclkid", "mtm_",
+    "ns_", "oly_anon_id", "oly_enc_id", "otc", "pcrid", "piwik_", "pk_",
+    "rb_clickid", "redirect_log_mongo_id", "redirect_mongo_id", "ref",
+    "s_kwcid", "sb_referer_host", "soc_src", "soc_trk", "spm", "sr_",
+    "srcid", "stm_", "trk_", "utm_", "vero_"
+]
+
+# https://stackoverflow.com/a/44645567/1418014
+URL_REGEX = re.compile(r"""
+    (
+        (?:
+            (?:https|http)?
+            :
+            (?:
+                /{1,3}
+                |
+                [a-z0-9%]
+            )
+            |
+            [a-z0-9.\-]+[.](?:com|org|uk)
+        /)
+        (?:
+            [^\s()<>{}\[\]]+
+            |
+            \([^\s()]*?\([^\s()]+\)[^\s()]*?\)
+            |
+            \([^\s]+?\)
+        )+
+        (?:
+            \([^\s()]*?\([^\s()]+\)[^\s()]*?\)
+            |
+            \([^\s]+?\)
+            |
+            [^\s`!()\[\]{};:'".,<>?«»“”‘’]
+        )
+        |
+        (?:
+            (?<!@)[a-z0-9]+
+            (?:
+                [.\-][a-z0-9]+
+            )*
+            [.](?:com|uk|ac)\b/?
+            (?!@)
+        )
+    )
+""", re.VERBOSE)
+
+
+def fix_url(url):
+    """Removes all querystring params with a prohibited prefix.
+    Call this with actual URLs only.
+    Must return text unchanged if there is no replacing to be done."""
+    parsed = urllib.parse.urlsplit(url)
+    qs = urllib.parse.parse_qs(parsed.query, keep_blank_values=True)
+    qs_keys = qs.keys()
+    ok_qs_keys = [
+        k for k in qs_keys
+        if not any([k.startswith(b) for b in STRIP_URL_QUERY_ELEMENTS_STARTS])
+    ]
+    if len(ok_qs_keys) == len(qs_keys): return url  # if removed nothing, bail
+    nqs = dict([(k, qs[k]) for k in ok_qs_keys])
+    parsed = parsed._replace(query=urllib.parse.urlencode(nqs, doseq=True))
+    return urllib.parse.urlunsplit(parsed)
+
+
+def fix_match_object(mo):
+    return fix_url(mo.group(0))
+
+
+def fix_text(text):
+    """Fixes all URLs within text."""
+    return URL_REGEX.sub(fix_match_object, text)
+
+
+def is_url(s):
+    "Only true if the passed s is exactly a URL and nothing else, no whitespace"
+    if type(s) is not str:
+        return False
+    return bool(URL_REGEX.match(s))
+
+
+class TestIsUrl(unittest.TestCase):
+    def test_nope(self):
+        self.assertFalse(is_url(None))
+        self.assertFalse(is_url("lol"))
+        self.assertFalse(is_url(9))
+        self.assertFalse(is_url("http"))
+        self.assertFalse(is_url("pants://example.com"))
+        self.assertFalse(is_url("hppp://example.com"))
+
+    def test_yep(self):
+        self.assertTrue(is_url("http://example.com"))
+        self.assertTrue(is_url("https://example.com"))
+        self.assertTrue(is_url("https://example.com?lol=1"))
+        self.assertTrue(is_url("https://kryogenix.org/days"))
+        self.assertTrue(is_url("http://example.com/a/b?utm_source=haha"))
+        self.assertTrue(is_url("https://google.com"))
+        self.assertTrue(is_url("https://nope.museum?param=123#frag1"))
+        self.assertTrue(is_url("https://nope.museum:8000?param=123#frag1"))
+        self.assertTrue(is_url("https://a:b@nope.museum:8000?param=123#frag1"))
+
+class TestFixUrl(unittest.TestCase):
+    def test_simple(self):
+        self.assertEqual(fix_url("lol"), "lol")
+        self.assertEqual(fix_url(None), None)
+
+    def test_unchanged(self):
+        self.assertEqual(fix_url("https://kryogenix.org/"),
+                         "https://kryogenix.org/")
+        self.assertEqual(fix_url("http://kryogenix.org/"),
+                         "http://kryogenix.org/")
+        self.assertEqual(fix_url("https://kryogenix.org/?untouched"),
+                         "https://kryogenix.org/?untouched")
+        self.assertEqual(fix_url("https://kryogenix.org/?untouched=ok"),
+                         "https://kryogenix.org/?untouched=ok")
+        self.assertEqual(fix_url("https://kryogenix.org/?a=1&b=2"),
+                         "https://kryogenix.org/?a=1&b=2")
+
+    def test_changed(self):
+        self.assertEqual(fix_url("https://kryogenix.org/?utm_source=bye"),
+                         "https://kryogenix.org/")
+        self.assertEqual(fix_url("https://kryogenix.org/?utm_source=bye&a=1"),
+                         "https://kryogenix.org/?a=1")
+        self.assertEqual(fix_url("https://kryogenix.org/?utm_source=bye&utm_media=banner"),
+                         "https://kryogenix.org/")
+        self.assertEqual(fix_url("https://kryogenix.org/?srcid=12345"),
+                         "https://kryogenix.org/")
+        # TrackerZapper's amusing test :-)
+        self.assertEqual(fix_url("https://www.youtube.com/watch?v=dQw4w9WgXcQ&s=never&fbclid=gunna&gclid=give&gclsrc=you&utm_content=up&utm_term=never&utm_campaign=gunna&utm_medium=let&utm_source=you&utm_id=down&_ga=never&mc_cid=gunna&mc_eid=run&_bta_tid=around&_bta_c=and&trk_contact=desert&trk_msg=you&trk_module=never&trk_sid=gunna&gdfms=make&gdftrk=you&gdffi=cry&_ke=never&redirect_log_mongo_id=gunna&redirect_mongo_id=say&sb_referer_host=goodbye&mkwid=never&pcrid=gunna&ef_id=tell&s_kwcid=a&msclkid=lie&dm_i=and&epik=hurt&pk_campaign=you"),
+                         "https://www.youtube.com/watch?v=dQw4w9WgXcQ&s=never")
+
+    def test_tricks(self):
+        self.assertEqual(fix_url("https://kryogenix.org/utm_source=bye/?a=1"),
+                         "https://kryogenix.org/utm_source=bye/?a=1")
+
+
+class TestExtractUrl(unittest.TestCase):
+    def test_simple(self):
+        self.assertEqual(URL_REGEX.findall("https://kryogenix.org"),
+                         ["https://kryogenix.org"])
+        self.assertEqual(URL_REGEX.findall("Testing https://kryogenix.org for urls"),
+                         ["https://kryogenix.org"])
+
+    def test_complex_urls(self):
+        self.assertEqual(URL_REGEX.findall("""
+        This is https://a:b@kryogenix.org:80/lol?a=b#frag1 here
+        """),
+                         ["https://a:b@kryogenix.org:80/lol?a=b#frag1"])
+
+    def test_multiple_urls(self):
+        self.assertEqual(URL_REGEX.findall("""
+        You can go to https://kryogenix.org/days or
+        http://example.com/a/b?utm_source=haha
+        or https://google.com or https://nope.museum?param=123#frag1 or
+        any other place you fancy
+        """),
+                         [
+                         "https://kryogenix.org/days",
+                         "http://example.com/a/b?utm_source=haha",
+                         "https://google.com",
+                         "https://nope.museum?param=123#frag1"
+                         ])
+
+class TestFixText(unittest.TestCase):
+    def test_simple(self):
+        self.assertEqual(fix_text("""
+            here is unchanged text
+        """),
+                         """
+            here is unchanged text
+        """)
+        self.assertEqual(fix_text("""
+            here is unchanged text with an unchanged url https://kryogenix.org
+        """),
+                         """
+            here is unchanged text with an unchanged url https://kryogenix.org
+        """)
+        self.assertEqual(fix_text("""
+            here is unchanged multiline text
+            with an unchanged url
+            https://kryogenix.org
+            right here
+        """),
+                         """
+            here is unchanged multiline text
+            with an unchanged url
+            https://kryogenix.org
+            right here
+        """)
+
+    def test_changed(self):
+        self.assertEqual(fix_text("""
+            here is unchanged multiline text
+            with a changed url
+            https://kryogenix.org?utm_source=no
+            right here
+        """),
+                         """
+            here is unchanged multiline text
+            with a changed url
+            https://kryogenix.org
+            right here
+        """)
+        self.assertEqual(fix_text("""
+            here is unchanged multiline text
+            with a changed url
+            https://kryogenix.org?utm_source=no&a=1
+            right here
+        """),
+                         """
+            here is unchanged multiline text
+            with a changed url
+            https://kryogenix.org?a=1
+            right here
+        """)
+
+
+if __name__ == "__main__":
+    unittest.main()
