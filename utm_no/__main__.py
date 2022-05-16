72104a360eb398e3a13908d96c4e67e1ca795c51
Stuart Langridge <sil@kryogenix.org>
2022-05-16 11:49:29 +0100

    fix invalid appstream

--- utm_no/__main__.py
+++ utm_no/__main__.py
@@ -41,7 +41,7 @@ SOFTWARE.
 
 """
 # Important that APP_VERSION line has = "(num)" in so gitcommit script finds it
-APP_VERSION = "1.60.31"
+APP_VERSION = "1.60.32"
 sv = os.environ.get("SNAP_VERSION")
 if sv:
     APP_VERSION = f"{sv} (snap)"
